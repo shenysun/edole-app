879c6703d09dfe192b8a292af049506f35628767
sunyongshen <sunyongshen@tal.com>
2023-04-18 15:47:01 +0800

    fix: 整理路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,26 +1,14 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-    // {
-    //     path: '/',
-    //     component: () => import('layouts/MainLayout.vue'),
-    //     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-    // },
-    //
-    // // Always leave this as last one,
-    // // but you can also remove it
-    // {
-    //     path: '/:catchAll(.*)*',
-    //     component: () => import('pages/ErrorNotFound.vue'),
-    // },
+    {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/error/index'),
+    },
     {
         path: '/',
         component: () => import('pages/main/index'),
     },
-    // {
-    //     path: '/',
-    //     component: () => import('pages/main/index'),
-    // },
 ];
 
 export default routes;
