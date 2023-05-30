2501b74c25f2403476270dd27ba552d8682aea86
shenysun <shenysun@163.com>
2023-05-30 19:04:15 +0800

    feat: 添加安装路径

--- build/build-installer.js
+++ build/build-installer.js
@@ -1,19 +1,41 @@
 const { createWindowsInstaller } = require('electron-winstaller');
 const { remove } = require('fs-extra');
 const path = require('path');
+const package = require('../package.json');
 
+// 获取windows是多少位
+const getWindowsArch = () => {
+    const arch = process.env.npm_config_arch || process.arch;
+    return arch === 'ia32' ? 'x86' : arch;
+};
+
+// 获取 platform
+const getPlatform = () => {
+    const platform = process.env.npm_config_platform || process.platform;
+    return platform === 'darwin' ? 'osx' : platform;
+};
+
+console.log(getWindowsArch());
 async function main() {
     const rootPath = path.join('./');
     const outPath = path.join(rootPath, 'dist/electron/Installer');
     remove(outPath);
-    console.log('开始打包...');
-    await createWindowsInstaller({
-        appDirectory: path.join(rootPath, 'dist/electron/Packaged/edole-app-win32-x64'),
-        noMsi: true,
-        outputDirectory: path.join(outPath, process.platform),
-        usePackageJson: true,
-    });
-    console.log('执行完毕');
+    const platform = getPlatform();
+    const exeDirname = `${package.name}-${platform}-${getWindowsArch()}`;
+    console.log('安装包目录：', exeDirname);
+
+    if (platform === 'win32') {
+        console.log('正在生成 windows 安装包...');
+        await createWindowsInstaller({
+            appDirectory: path.join(rootPath, 'dist/electron/Packaged/' + exeDirname),
+            noMsi: true,
+            outputDirectory: path.join(outPath, process.platform),
+            usePackageJson: true,
+            loadingGift: path.join(rootPath, 'build/loading.gif'),
+        });
+    } else if (platform === 'darwin') {
+    }
+    console.log('生成完毕');
 }
 
 main();
--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -4,99 +4,109 @@ import os from 'os';
 import ShellAction from './actions/ShellAction';
 import fixPath from 'fix-path';
 import { AppMenu } from './menu/AppMenu';
+const checked = require('electron-squirrel-startup');
 
-fixPath();
-// needed in case process is undefined under Linux
-const platform = process.platform || os.platform();
-
-try {
-    if (platform === 'win32' && nativeTheme.shouldUseDarkColors) {
-        require('fs').unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'));
+function init() {
+    if (checked) {
+        return;
     }
-} catch (_) {}
+    fixPath();
+    // needed in case process is undefined under Linux
+    const platform = process.platform || os.platform();
 
-let mainWindow: BrowserWindow | undefined;
+    try {
+        if (platform === 'win32' && nativeTheme.shouldUseDarkColors) {
+            require('fs').unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'));
+        }
+    } catch (_) {}
 
-function initApp() {
-    createWindow();
-    new AppMenu(platform);
-}
+    let mainWindow: BrowserWindow | undefined;
 
-function createWindow() {
-    /**
-     * Initial window options
-     */
-    const displays = screen.getAllDisplays();
-    const display =
-        displays.find((display) => {
-            return display.bounds.x !== 0 || display.bounds.y !== 0;
-        }) || screen.getPrimaryDisplay();
+    function initApp() {
+        createWindow();
+        new AppMenu(platform);
+    }
 
-    let bounds = {
-        x: 0,
-        y: 0,
-        w: 0,
-        h: 0,
-    };
-    const scale = 1 / 2;
-    if (display) {
-        const w = Math.round(display.workAreaSize.width * scale);
-        const h = Math.round(display.workAreaSize.height * scale);
-        const x = Math.round(display.bounds.x + (display.workAreaSize.width - display.workAreaSize.width * scale) / 2);
-        const y = Math.round(
-            display.bounds.y + (display.workAreaSize.height - display.workAreaSize.height * scale) / 2
-        );
-        bounds = {
-            x,
-            y,
-            w,
-            h,
+    function createWindow() {
+        /**
+         * Initial window options
+         */
+        const displays = screen.getAllDisplays();
+        const display =
+            displays.find((display) => {
+                return display.bounds.x !== 0 || display.bounds.y !== 0;
+            }) || screen.getPrimaryDisplay();
+
+        let bounds = {
+            x: 0,
+            y: 0,
+            w: 0,
+            h: 0,
         };
+        const scale = 1 / 2;
+        if (display) {
+            const w = Math.round(display.workAreaSize.width * scale);
+            const h = Math.round(display.workAreaSize.height * scale);
+            const x = Math.round(
+                display.bounds.x + (display.workAreaSize.width - display.workAreaSize.width * scale) / 2
+            );
+            const y = Math.round(
+                display.bounds.y + (display.workAreaSize.height - display.workAreaSize.height * scale) / 2
+            );
+            bounds = {
+                x,
+                y,
+                w,
+                h,
+            };
+        }
+
+        mainWindow = new BrowserWindow({
+            x: bounds.x,
+            y: bounds.y,
+            icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
+            width: bounds.w,
+            height: bounds.h,
+            useContentSize: true,
+            webPreferences: {
+                contextIsolation: true,
+                // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
+                preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
+            },
+        });
+
+        new ShellAction(mainWindow);
+
+        mainWindow.loadURL(process.env.APP_URL);
+
+        if (process.env.DEBUGGING) {
+            // if on DEV or Production with debug enabled
+            // mainWindow.webContents.openDevTools();
+        } else {
+            // we're on production; no access to devtools pls
+            // mainWindow.webContents.on('devtools-opened', () => {
+            //     mainWindow?.webContents.closeDevTools();
+            // });
+        }
+
+        mainWindow.on('closed', () => {
+            mainWindow = undefined;
+        });
     }
 
-    mainWindow = new BrowserWindow({
-        x: bounds.x,
-        y: bounds.y,
-        icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
-        width: bounds.w,
-        height: bounds.h,
-        useContentSize: true,
-        webPreferences: {
-            contextIsolation: true,
-            // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
-            preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
-        },
+    app.whenReady().then(initApp);
+
+    app.on('window-all-closed', () => {
+        if (platform !== 'darwin') {
+            app.quit();
+        }
     });
 
-    new ShellAction(mainWindow);
-
-    mainWindow.loadURL(process.env.APP_URL);
-
-    if (process.env.DEBUGGING) {
-        // if on DEV or Production with debug enabled
-        // mainWindow.webContents.openDevTools();
-    } else {
-        // we're on production; no access to devtools pls
-        // mainWindow.webContents.on('devtools-opened', () => {
-        //     mainWindow?.webContents.closeDevTools();
-        // });
-    }
-
-    mainWindow.on('closed', () => {
-        mainWindow = undefined;
+    app.on('activate', () => {
+        if (mainWindow === undefined) {
+            initApp();
+        }
     });
 }
 
-app.whenReady().then(initApp);
-
-app.on('window-all-closed', () => {
-    if (platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (mainWindow === undefined) {
-        initApp();
-    }
-});
+init();
